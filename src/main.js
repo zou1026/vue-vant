b2189f410abf4869509a3fa38100ad7873561122
zoushiyang <315360734@qq.com>
2020-10-16 16:40:20 +0800

    111

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// 添加 IE 兼容
+import 'core-js/stable'
+import 'regenerator-runtime/runtime' // 将es6抓换成es5
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 设置 js中可以访问 $cdn
+import { $cdn } from '@/config'
+Vue.prototype.$cdn = $cdn
+
+// 全局引入按需引入UI库 vant
+import '@/plugins/vant'
+// 引入全局样式
+import '@/assets/css/index.scss'
+// 移动端适配
+import 'lib-flexible/flexible.js'
+
+// filters
+import './filters'
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
